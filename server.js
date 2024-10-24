85f6958c5ce71e099d89ad27e2cde31914f391a0
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2024-10-24 09:08:46 -0300

    Updating Cross Origin configuration

--- server.js
+++ server.js
@@ -20,21 +20,12 @@ const app = express();
 app.set("secretKey", "alura");
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors())
-
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "X-Requested-With,content-type"
-    );
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
+app.use(cors({
+    origin: 'https://dex.rweb.site',
+    credentials: true,
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['X-Requested-With', 'content-type']
+}));
 
 let threadId;
 
