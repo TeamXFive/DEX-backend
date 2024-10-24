56ba478194a6f96daff8f1162da5c27ca46b535e
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2024-10-24 08:37:50 -0300

    Requiring cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const upload = require('./fileUpload.js');
 const fs = require('fs');
+var cors = require('cors')
 
 const express = require("express");
 const dotenv = require("dotenv");
@@ -19,6 +20,7 @@ const app = express();
 app.set("secretKey", "alura");
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors())
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
